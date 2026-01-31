d6bbe03c0a242dfb8fdd1f32241cb4e033b7db5c
Sander Løtuft <sander.lotuft@bkk.no>
2026-01-31 23:55:21 +0100

    idk

--- src/data/level5.ts
+++ src/data/level5.ts
@@ -10,7 +10,16 @@ const level5Green: Level = {
     { x: -50, y: -480, width: 2550, height: 500, breakable: false, invisible: false },
     { x: -50, y: 0, width: 70, height: 1500, breakable: false, invisible: false },
     { x: 2440, y: 0, width: 70, height: 1500, breakable: false, invisible: false },
-    { x: -50, y: 1000, width: 2550, height: 500, breakable: false, invisible: false },
+    { x: -50, y: 1000, width: 400, height: 500, breakable: false, invisible: false },
+
+    // Level platforms
+    { x: 500, y: 850, width: 1500, height: 150, breakable: false, invisible: false },
+    
+    { x: 1000, y: 700, width: 500, height: 150, breakable: false, invisible: false },
+
+    // Invisible platforms
+    { x: 100, y: 400, width: 200, height: 20, breakable: false, invisible: true },
+    { x: 350, y: 1200, width: 800, height: 20, breakable: false, invisible: true },
   ],
   goals: [
     {
